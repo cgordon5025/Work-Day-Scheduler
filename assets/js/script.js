1f01665c054cc728208cb6e18e3f24d47f545655
Charlee Gordon <cgordon5025@gmail.com>
2022-09-21 21:49:36 -0400

    final work for the night

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var today = moment();
-$("#todayDate").text(today.format("dddd, MMMM do YYYY"))
+$("#todayDate").text(today.format("dddd, MMMM do YYYY"));
 
+var currentTime = parseInt(moment().format('H'));
 
 var saveBtn = document.getElementById("save9")
 
@@ -9,7 +10,7 @@ var currentItem;
 var inputEL = document.getElementById("9amEvent")
 saveBtn.addEventListener("click", SaveEvent)
 
-
+var timeBlocks = document.querySelectorAll("input")
 
 function SaveEvent(event) {
     event.preventDefault()
@@ -22,4 +23,27 @@ function renderEvent() {
     savedEvent = JSON.parse(localStorage.getItem("myEvent"))
     inputEL.textContent = savedEvent
 }
+//we want this to show at all times
+renderEvent()
 //I want to be able to save each item and put it in the respective space,way to do it without making code explode?
+
+//This will change color based upon what time the event is supposed to occur and the current time
+Array.from(rows).forEach(row => {
+    let eventIdString = row.id
+    eventHour;
+    if (eventIdString) {
+        eventHour = parseInt(eventIdString)
+    };
+    if (eventHour) {
+        if (currentTime === eventHour) {
+            timeBlocks.css({ 'background-color': '#ff6961', 'color': 'white' });
+        } else if ((currentTime < eventHour) && (currentTime > eventHour - 6)) {
+            timeBlocks.css({ 'background-color': '#77dd77', 'color': 'white' });
+        } else if ((currentTime > eventHour) && (currentTime < eventHour + 6)) {
+            timeBlocks.css({ 'background-color': '#d3d3d3', 'color': 'white' })
+        } else {
+            timeBlocks.css({ 'background-color': 'white' })
+        }
+    }
+})
+
