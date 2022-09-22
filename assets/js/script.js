5476e3908b853499ca056f8d5c58be475e4d71c3
Charlee Gordon <cgordon5025@gmail.com>
2022-09-21 20:43:41 -0400

    haave small version up and formatted, need to learn more tomorrow to continue on this

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
 var today = moment();
 $("#todayDate").text(today.format("dddd, MMMM do YYYY"))
+
+
+var saveBtn = document.getElementById("save9")
+
+var saveArray = []
+var currentItem;
+var inputEL = document.getElementById("9amEvent")
+saveBtn.addEventListener("click", SaveEvent)
+
+
+
+function SaveEvent(event) {
+    event.preventDefault()
+    currentItem = inputEL.value;
+    localStorage.setItem("myEvent", JSON.stringify(currentItem))
+    renderEvent()
+}
+
+function renderEvent() {
+    savedEvent = JSON.parse(localStorage.getItem("myEvent"))
+    inputEL.textContent = savedEvent
+}
+//I want to be able to save each item and put it in the respective space,way to do it without making code explode?
